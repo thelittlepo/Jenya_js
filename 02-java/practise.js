805c86e0b337c57cc6e85fa7a894fee1643f198b
Afanasyeva Polina <NotBadPolly@yandex.ru>
2019-03-05 11:04:21 +0300

    arrays finished

--- 02-java/practise.js
+++ 02-java/practise.js
@@ -40,15 +40,15 @@
 
 
 
-function nextEvent(num) {
-  if (num % 2 === 0) {
-    return num + 2
-  } else {
-    return num + 1
-  }
-}
-let num = 10
-console.log("Вызываю функцию nextEvent(num): " + nextEvent(num));
+// function nextEvent(num) {
+//   if (num % 2 === 0) {
+//     return num + 2
+//   } else {
+//     return num + 1
+//   }
+// }
+// let num = 10
+// console.log("Вызываю функцию nextEvent(num): " + nextEvent(num));
 
 // function makeDiff(a, b, c) {
 //   if (Math.min([2 [ 1[ 3 ]]]) {
@@ -59,16 +59,55 @@ console.log("Вызываю функцию nextEvent(num): " + nextEvent(num));
 // let c = 3
 // console.log("Вызываю функцию makeDiff(a, b, c): " + makeDiff(a, b, c));
 
-function calcPrice(price, goods) {
-  return price * goods
-}
-let price = 20
-let goods = 10
-console.log("Вызываю функцию calcPrice(price, goods): " + calcPrice(price, goods));
+// function calcPrice(price, goods) {
+//   return price * goods
+// }
+// let price = 20
+// let goods = 10
+// console.log("Вызываю функцию calcPrice(price, goods): " + calcPrice(price, goods));
+//
+// function splitPrice(price, value) {
+//   return price / value
+// }
+// let price = 300
+// let value = 15
+// console.log("Вызываю функцию splitPrice(price, value): " + splitPrice(price, value));
 
-function splitPrice(price, value) {
-  return price / value
-}
-let price = 300
-let value = 15
-console.log("Вызываю функцию splitPrice(price, value): " + splitPrice(price, value));
+
+let scores = [22, 33, 49, 87, 82, 38, 18, 49, 87, 49, 73, 18, 56, 62]
+
+// let i = 0
+//   while (i < scores.length) {
+//     if (scores[i] > highScore) {
+//       highScore = scores[i]
+//     }
+//     let msg = `Лошадь №${i + 1} пришла с результатом ${scores[i]}`
+//     console.log(msg)
+//     i += 1
+//   }
+  // console.log(highScore)
+
+  // let j = 0
+  // while (j < scores.length) {
+  //   if(score[j] === highScore) {
+  //     bestScores.push(scores[j])
+  //   }
+  // }
+  let highScore = 0
+  for (let i = 0; i < scores.length; i++) {
+    if (scores[i] > highScore) {
+      highScore = scores[i]
+    }
+    let msg = `Лошадь №${i + 1} пришла с результатом ${scores[i]}`
+    console.log(msg)
+    i += 1
+  }
+  console.log(highScore);
+  
+  let bestScores = []
+  for (let j = 0; j < scores.length; j++) {
+    if (scores[j] === highScore) {
+      bestScores.push(scores[j])
+    }
+  }
+  console.log(bestScores);
